c68e4214f112533010e8b053af50ff889e4cbbb1
sduhubert <husyl23@student.sdu.dk>
2025-12-04 01:12:16 +0100

    fix

--- travel_together_app/travel_together/__init__.py
+++ travel_together_app/travel_together/__init__.py
@@ -21,8 +21,8 @@ def create_app(test_config=None):
     # Code to place inside create_app, after the other app.config assignment
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = "mysql+pymysql://traveltogether:waDBlog@localhost/TravelTogether"
-    #] = "mysql+pymysql://26_webapp_00:Tq6qlpko@mysql.lab.it.uc3m.es/26_webapp_00b"
+    #] = "mysql+pymysql://traveltogether:waDBlog@localhost/TravelTogether"
+    ] = "mysql+pymysql://26_webapp_00:Tq6qlpko@mysql.lab.it.uc3m.es/26_webapp_00b"
     
     # app.config[
     #   "SQLALCHEMY_DATABASE_URI"
